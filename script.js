ec6596d3195e5e29906fa8d7ebbc1a53f2ccea3b
pucur12 <akmalhaydir@gmail.com>
2023-04-03 15:08:02 +0800

    Mengulang Karna error

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
         loop: true
     });
 
-    // owl carousell script
+    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
